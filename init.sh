c0f15c6af49791928c7e5e10d5232a0dfb8198e7
Dolu1990 <charles.papon.90@gmail.com>
2022-01-27 13:25:06 +0100

    Fix init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 mkdir -p buildroot/images
-cd buildroot/images
 gdown --folder -O buildroot/images/rv32ima https://drive.google.com/drive/folders/1mf-OzlZA5_FMLM3ENIeL3PEcVtlh3QlK?usp=sharing
