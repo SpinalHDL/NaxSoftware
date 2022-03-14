aed3c7749bc8ff547298521a54b5e1aac4d10c73
Dolu1990 <charles.papon.90@gmail.com>
2022-03-14 12:46:54 +0100

    Coremark now use mediany and signed ee_u32 (LOL)

--- baremetal/coremark/naxriscv_sim/core_portme.h
+++ baremetal/coremark/naxriscv_sim/core_portme.h
@@ -96,8 +96,8 @@ typedef unsigned short ee_u16;
 typedef signed int     ee_s32;
 typedef double         ee_f32;
 typedef unsigned char  ee_u8;
-typedef unsigned int   ee_u32;
-typedef ee_u32         ee_ptr_int;
+typedef int            ee_u32;
+typedef long           ee_ptr_int;
 typedef size_t         ee_size_t;
 /* align_mem :
         This macro is used to align an offset to point to a 32b value. It is
