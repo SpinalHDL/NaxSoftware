b899f6134c80540ea1a0873d39f436ed0506265c
Charles Papon <charles.papon.90@gmail.com>
2022-05-03 23:34:27 +0200

    fix gdown

--- init.sh
+++ init.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-mkdir -p buildroot/images
-gdown --folder -O buildroot/images/rv32ima https://drive.google.com/drive/folders/1mf-OzlZA5_FMLM3ENIeL3PEcVtlh3QlK?usp=sharing
-gdown --folder -O buildroot/images/rv64ima https://drive.google.com/drive/folders/1o1Kpz2PnIkILugjcOQ5RJC23FL_XyaTL?usp=sharing
-gdown --folder -O buildroot/images/rv32imac https://drive.google.com/drive/folders/1hJJV9uDgBsiX3SxQU8wBR21JWd8q9DLN?usp=sharing
-gdown --folder -O buildroot/images/rv64imac https://drive.google.com/drive/folders/1lNO3joEfBoI_YoF2pv-uZ7JCyxPRxfqb?usp=sharing
+gdown --fuzzy  https://drive.google.com/file/d/1f-YFm6I185R9o_NniNBJkkVGNghRBEDK/view?usp=sharing
+unzip buildroot.zip
+
