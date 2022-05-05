5e643428b29b559d58da45c34e78f32531f4b544
Charles Papon <charles.papon.90@gmail.com>
2022-05-05 20:10:32 +0200

    Avoid gdown

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-gdown --fuzzy  https://drive.google.com/file/d/1f-YFm6I185R9o_NniNBJkkVGNghRBEDK/view?usp=sharing
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1f-YFm6I185R9o_NniNBJkkVGNghRBEDK' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1f-YFm6I185R9o_NniNBJkkVGNghRBEDK" -O buildroot.zip && rm -rf /tmp/cookies.txt
 unzip buildroot.zip
 
