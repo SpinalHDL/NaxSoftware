90a189a7fa3bf05ff29e0e562ad95bac663e8c64
Dolu1990 <charles.papon.90@gmail.com>
2022-06-16 16:03:09 +0200

    Add FPU test

--- baremetal/common/all_arch.mk
+++ baremetal/common/all_arch.mk
@@ -1,8 +1,12 @@
 
 
-ARCHS=rv32im rv32imac rv64im rv64imac
+ARCHS=rv32im rv32imac rv32imafd rv32imacfd rv64im rv64imac rv64imafd rv64imacfd
 rv32im=MARCH=rv32im MABI=ilp32
 rv32imac=MARCH=rv32imac MABI=ilp32
+rv32imafd=MARCH=rv32imafd MABI=ilp32
+rv32imacfd=MARCH=rv32imacfd MABI=ilp32
 rv64im=MARCH=rv64im MABI=lp64
 rv64imac=MARCH=rv64imac MABI=lp64
+rv64imafd=MARCH=rv64imafd MABI=lp64
+rv64imacfd=MARCH=rv64imacfd MABI=lp64
 
