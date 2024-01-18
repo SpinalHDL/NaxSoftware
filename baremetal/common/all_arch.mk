06644a7a9f399780a5a16747764685a7a335fef4
Dolu1990 <charles.papon.90@gmail.com>
2024-01-18 13:44:47 +0100

    add rv32i rv64i

--- baremetal/common/all_arch.mk
+++ baremetal/common/all_arch.mk
@@ -1,24 +1,26 @@
 ARCHS=
 ifneq ($(RV32),no)
 ifneq ($(NEED_FPU),yes)
-ARCHS+=rv32ima rv32imac
+ARCHS+=rv32i rv32ima rv32imac
 endif
 ARCHS+=rv32imaf rv32imafc rv32imafd rv32imafdc
 endif
 
 ifneq ($(RV64),no)
 ifneq ($(NEED_FPU),yes)
-ARCHS+=rv64ima rv64imac
+ARCHS+=rv64i rv64ima rv64imac
 endif
 ARCHS+=rv64imaf rv64imafc rv64imafd rv64imafdc
 endif
 
+rv32i=MARCH=rv32i MABI=ilp32
 rv32ima=MARCH=rv32ima MABI=ilp32
 rv32imac=MARCH=rv32imac MABI=ilp32
 rv32imaf=MARCH=rv32imaf MABI=ilp32f
 rv32imafc=MARCH=rv32imafc MABI=ilp32f
 rv32imafd=MARCH=rv32imafd MABI=ilp32d
 rv32imafdc=MARCH=rv32imafdc MABI=ilp32d
+rv64i=MARCH=rv64i MABI=lp64
 rv64ima=MARCH=rv64ima MABI=lp64
 rv64imac=MARCH=rv64imac MABI=lp64
 rv64imaf=MARCH=rv64imaf MABI=lp64f
