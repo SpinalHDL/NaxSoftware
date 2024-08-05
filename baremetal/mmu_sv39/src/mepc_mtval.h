bdaabe78c70fb12a04d5ba4eeef4a3c9ad479dd0
Dolu1990 <charles.papon.90@gmail.com>
2024-08-05 11:32:47 +0200

    mmu_sv39 now trust spike

--- baremetal/mmu_sv39/src/mepc_mtval.h
+++ baremetal/mmu_sv39/src/mepc_mtval.h
@@ -2,27 +2,27 @@
 #define test_trap(code) \
     supervisor_setup_trap;\
     li x1, 1 << 36; code;\
-    supervisor_handle_trap;\
+    supervisor_handle_trap_or_pass;\
 ;\
     supervisor_setup_trap;\
     li x1, 1 << 37; code;\
-    supervisor_handle_trap;\
+    supervisor_handle_trap_or_pass;\
 ;\
     supervisor_setup_trap;\
     li x1, 1 << 38; code;\
-    supervisor_handle_trap;\
-    ;\
+    supervisor_handle_trap_or_pass;\
+;\
     supervisor_setup_trap;\
     li x1, ~((1 << 37)-1); code;\
-    supervisor_handle_trap;\
+    supervisor_handle_trap_or_pass;\
 ;\
     supervisor_setup_trap;\
     li x1, ~((1 << 38)-1); code;\
-    supervisor_handle_trap;\
+    supervisor_handle_trap_or_pass;\
 ;\
     supervisor_setup_trap;\
     li x1, ~((1 << 39)-1); code;\
-    supervisor_handle_trap;\
+    supervisor_handle_trap_or_pass;\
 
 #define LOAD lw x1, 0(x1)
 #define STORE sw x1, 0(x1)
