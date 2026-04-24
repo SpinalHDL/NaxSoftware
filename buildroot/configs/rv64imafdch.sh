cf40c730b3cf6e7215f4c3c7a7395b98ab005987
Inochi Amaoto <inochiama@gmail.com>
2026-04-24 10:07:15 +0800

    buildroot: Add RVH based build file and configuration
    Signed-off-by: Inochi Amaoto <inochiama@gmail.com>

---
+++ buildroot/configs/rv64imafdch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ISA=(i m a f d c h zicsr zifence)
+FLAGS+=" -DISA_BASE=\"rv64i\""
+FLAGS+=" -DXLEN=64"
+FLAGS+=" -DABI=lp64d"
+FLAGS+=" -DMMU_TYPE=\"riscv,sv39\""
+FLAGS+=" -DRV64"
+FLAGS+=" -DRVC=y"
+FLAGS+=" -DFPU=y"
+FLAGS+=" -DRVH=y"
